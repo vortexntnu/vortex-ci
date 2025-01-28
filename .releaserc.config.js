dde2e02f5bc84d1b9d336223a47ff582153f9a24
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-01-28 22:54:53 +0100

    ci(semrel): test 12

--- .releaserc.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    branches: [process.env.DEFAULT_BRANCH],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/github"
-    ]
-};
