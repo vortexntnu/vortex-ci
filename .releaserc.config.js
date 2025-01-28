8d78116ca7e4dd4ce766c7ee3d90e31f48aeff54
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-01-28 22:21:21 +0100

    ci(semrel): test 6

--- .releaserc.config.js
+++ .releaserc.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["master"], // Fallback to "main" if DEFAULT_BRANCH is not set
+    branches: ["apple"], // Fallback to "main" if DEFAULT_BRANCH is not set
     plugins: [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
